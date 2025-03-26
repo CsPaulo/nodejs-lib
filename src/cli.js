e4315ce3b8137aff4bf0f42fc2c8cec9c85fc801
CsPaulo <cspaulorafael@gmail.com>
2025-03-26 14:22:31 -0300

    feat: implement CLI options for file processing and result saving

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,49 @@
 import fs from "fs";
+import path from "path";
 import trataErros from "./erros/funcoesErro.js";
 import { contaPalavras } from "./index.js";
 import { montaSaidaArquivo } from "./helpers.js";
+import { Command } from "commander";
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const program = new Command();
+program
+  .version("0.0.1")
+  .description("Conta palavras em um arquivo")
+  .option("-t, --texto <string>", "Caminho do texto a ser processado")
+  .option("-d, --destino <string>", "caminho da pasta onde salvar o arquivo de resultados")
+  .action((options) => {
+    const { texto, destino } = options;
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-  try {
-    if (erro) throw erro;
-    const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, endereco);
-  } catch (erro) {
-    trataErros(erro);
-  }
-});
+    if (!texto || !destino) {
+      console.error("erro: favor informar o caminho do arquivo e o destino");
+      program.help();
+      return;
+    }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log("Texto processado com sucesso");
+    } catch (erro) {
+      console.error("Ocorreu um erro no processamento", erro);
+    }
+  });
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
